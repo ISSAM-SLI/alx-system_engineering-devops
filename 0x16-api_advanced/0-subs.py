ae4bd4f73ffdb51145c34f63fa34abec6a186a93
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-08-13 12:30:23 -0700

    Tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,10 @@ Module for querying Reddit API to get subscriber count.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    '''Queries the Reddit API and returns the number of subscribers for the given subreddit.
+    '''Queries the Reddit API and returns the number
+       of subscribers for the given subreddit.
        Returns 0 if the subreddit is invalid or an error occurs.
     '''
     headers = {'User-Agent': 'test'}
