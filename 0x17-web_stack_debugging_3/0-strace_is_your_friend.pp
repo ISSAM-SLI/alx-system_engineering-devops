9a2ed4f55b74374c0099df3bad75ed558592a510
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-08-14 13:03:17 -0700

    Tasks

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Correct typo 'phpp' to 'php' in the WordPress wp-settings.php file
+exec { 'fix setting':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
