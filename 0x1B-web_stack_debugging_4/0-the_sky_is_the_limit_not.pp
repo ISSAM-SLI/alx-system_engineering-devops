fa9c3de435a7e5b71e1759770b8ce10c1a5287f1
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-08-20 18:55:16 -0700

    Tasks

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Updates Nginx file descriptor limit and restarts the service
+exec { 'fix file limit':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
